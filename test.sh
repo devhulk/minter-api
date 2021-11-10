6a41c0a3befc6d692448861fb785124195769fa9
devhulk <gry2big93@gmail.com>
2021-11-09 23:17:24 -0500

    mint asset endpoint

--- test.sh
+++ test.sh
@@ -11,7 +11,7 @@ curl -X POST -H "Content-Type: application/json" \
 
 # Test mint asset endpoint
 curl -X POST -H "Content-Type: application/json" \
-    -d '{"address": "addr_test1qzjy75c3tyyvl3t92y9404hdaqnhpcuxedqchyyuvg3phymryqfyt540zetndfm7u707afmn6ptg6vyuh7axve44sgwssgw298", "config": "testnet", "metadata": { "asset_id": "testID", "asset_name": "testName", "ipfsLink": "ipfs://test", "amount": "1", "traits": [{"head" : "original"}] }}' \
+    -d '{"address": "addr_test1vpfvdy0rvkawm6zz4l3y5fykyagp5r7g300xv7dhrkxs4aq8mt5vq", "config": "testnet", "metadata": { "asset_id": "testID", "asset_name": "testName", "ipfsLink": "ipfs://test", "amount": "1", "traits": [{"head" : "original"}] }}' \
     http://localhost:3572/v1/cardano/mint/asset
 
 # Test mint endpoint
