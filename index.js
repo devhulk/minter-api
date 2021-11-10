da1c731218a3cd6c1d490dbab945f198e6bbc5f9
devhulk <gry2big93@gmail.com>
2021-11-09 19:25:48 -0500

    mint asset endpoint

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.post('/v1/cardano/mint/asset', function (req, res) {
             .then(() => {
                 minter.getMetaData(mintData)
                 .then((metadata) => {
-                    mintData.metadata = metadata
+                    mintData.metadata = metadata.trim()
                     res.send(mintData)
                 })
             })
