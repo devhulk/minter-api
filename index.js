5bc0826b1c114ca71e78092533e02ab5eea89c35
devhulk <gry2big93@gmail.com>
2021-11-10 23:28:59 -0500

    added transactions, refactor

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ import axios from 'axios'
 // let client = new Cardano()
 
 app.post('/v1/cardano/transactions', function (req, res) {
-    let network = req.body.network
-    axios.get(`https://cardano-${network}.blockfrost.io/api/v0/addresses/${req.body.address}/transactions`, {headers: {'project_id': 'testnetxR0g77qOcoQ9CZbE5TOrYstSzERzVFef'}})
+    axios.get(`https://cardano-testnet.blockfrost.io/api/v0/addresses/addr_test1vpfvdy0rvkawm6zz4l3y5fykyagp5r7g300xv7dhrkxs4aq8mt5vq/transactions`, {headers: {'project_id': 'testnetxR0g77qOcoQ9CZbE5TOrYstSzERzVFef'}})
     .then((response) => {
         res.send(response)
     })
