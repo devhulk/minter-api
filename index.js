ea6699fbf2e4a5adcd0fa68e1daee8e7433e93c0
devhulk <gry2big93@gmail.com>
2021-11-11 21:14:22 -0500

    mongodb integration

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.post('/v1/cardano/txs/utxos', function (req, res) {
         console.log(response.data)
         let input = response.data.inputs[0]
         let output = response.data.outputs[0]
-        let amount = output.amount[0]
+        let amount = input.amount[0]
         let amountRecieved = {address: input.address , amount: amount.quantity / 1000000, fullData: response.data }
         res.send(amountRecieved)
     })
