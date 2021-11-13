70954154be0ac3f161610a3ad190bbdd58c89203
devhulk <gry2big93@gmail.com>
2021-11-13 17:49:34 -0500

    get minted

--- src/Transactions/index.js
+++ src/Transactions/index.js
@@ -17,7 +17,7 @@ export default class Transactions {
                     let payments = []
                     txs.forEach((tx) => {
                         if (tx.address == options.mintWalletAddr) {
-                           payments.push(payments) 
+                           payments.push(tx) 
                         }
                     }) 
                     resolve(payments)
