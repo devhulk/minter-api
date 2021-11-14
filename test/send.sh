94e7dc8557c1d053fb68ad08fa09a2a44d673ad4
devhulk <gry2big93@gmail.com>
2021-11-13 23:44:06 -0500

    seperated mint and send

--- test/send.sh
+++ test/send.sh
@@ -2,4 +2,4 @@
 
 curl -X POST -H "Content-Type: application/json" \
     -d @testID3.json \
-    http://localhost:3572/v1/cardano/mint/asset | jq
+    http://localhost:3572/v1/cardano/mint/sendAsset | jq
