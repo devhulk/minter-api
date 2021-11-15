f48ef6d7efc9fc1bb57a57790c84feee229cc696
devhulk <gry2big93@gmail.com>
2021-11-15 13:17:49 -0500

    drop day

--- minter-api.js
+++ minter-api.js
@@ -8,31 +8,31 @@ app.use(cors({
     origin: '*'
 }))
 app.use(bodyParser.json())
-// app.use(expressWinston.logger({
-//     transports: [
-//       new winston.transports.Console()
-//     ],
-//     format: winston.format.combine(
-//       winston.format.colorize(),
-//       winston.format.json()
-//     ),
-//     meta: true, // optional: control whether you want to log the meta data about the request (default to true)
-//     msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
-//     expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
-//     colorize: false, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
-//     // ignoreRoute: function (req, res) { return false; } // optional: allows to skip some log messages based on request and/or response
-//   }));
+app.use(expressWinston.logger({
+    transports: [
+      new winston.transports.Console()
+    ],
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.json()
+    ),
+    meta: true, // optional: control whether you want to log the meta data about the request (default to true)
+    msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+    expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
+    colorize: false, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
+    // ignoreRoute: function (req, res) { return false; } // optional: allows to skip some log messages based on request and/or response
+  }));
 
-  // express-winston errorLogger makes sense AFTER the router.
-//   app.use(expressWinston.errorLogger({
-//     transports: [
-//       new winston.transports.Console()
-//     ],
-//     format: winston.format.combine(
-//       winston.format.colorize(),
-//       winston.format.json()
-//     )
-//   }));
+//   express-winston errorLogger makes sense AFTER the router.
+  app.use(expressWinston.errorLogger({
+    transports: [
+      new winston.transports.Console()
+    ],
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.json()
+    )
+  }));
 
 
 // import Cardano from "./src/Cardano";
