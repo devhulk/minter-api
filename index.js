b836a3c8b5c3e691e04270194ffd2dff60a1e420
devhulk <gry2big93@gmail.com>
2021-11-14 20:21:32 -0500

    seperated mint and send

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 import express from 'express'
 import cors from 'cors'
+let winston = require('winston'),
+    expressWinston = require('express-winston');
 const bodyParser = require('body-parser')
 const app = express()
 app.use(cors({
     origin: '*'
 }))
-
 app.use(bodyParser.json())
+app.use(expressWinston.logger({
+    transports: [
+      new winston.transports.Console()
+    ],
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.json()
+    ),
+    meta: true, // optional: control whether you want to log the meta data about the request (default to true)
+    msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+    expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
+    colorize: true, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
+    // ignoreRoute: function (req, res) { return false; } // optional: allows to skip some log messages based on request and/or response
+  }));
+
+  // express-winston errorLogger makes sense AFTER the router.
+  app.use(expressWinston.errorLogger({
+    transports: [
+      new winston.transports.Console()
+    ],
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.json()
+    )
+  }));
+
 
 // import Cardano from "./src/Cardano";
 import Minter from "./src/Mint"
