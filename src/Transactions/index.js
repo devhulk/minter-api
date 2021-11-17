07c60f78bce1d2ed2fbc4cd995a09290b1c66743
devhulk <gry2big93@gmail.com>
2021-11-17 18:06:38 -0500

    added getAllUTXOS from drop monitor to api

--- src/Transactions/index.js
+++ src/Transactions/index.js
@@ -66,21 +66,7 @@ export default class Transactions {
                 // return JSON.stringify(utxos)
                 return utxos
             })
-            .catch(function (error) {
-                if (error.response) {
-                  // Request made and server responded
-                  console.log(error.response.data);
-                  console.log(error.response.status);
-                  console.log(error.response.headers);
-                } else if (error.request) {
-                  // The request was made but no response was received
-                  console.log(error.request);
-                } else {
-                  // Something happened in setting up the request that triggered an Error
-                  console.log('Error', error.message);
-                }
-            
-              });
+            .catch(function (error) { console.log(error)})
 
               return allUTXOS
     }
