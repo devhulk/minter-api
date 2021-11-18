9d5b0b2edfba9a6edc019ff79f30dbb483b1921c
devhulk <gry2big93@gmail.com>
2021-11-18 12:42:02 -0500

    added getAllUTXOS from drop monitor to api

--- src/TXInputs/inputs.js
+++ src/TXInputs/inputs.js
@@ -7,21 +7,17 @@ export default class TXInputs {
     }
 
     get() {
-        if (this.txinputs.length > 1) {
             let inputs = this.txinputs.map((input) => {
                 let txinput = new TXInput(input)
                 return txinput
             })
 
             return inputs
-        } else {
-            return new TXInput(this.txinputs[0])
-        }
 
     }
 
     getFirst() {
-        let inputs = this.get()
+        let inputs = this.txinputs 
 
         return inputs[0]
     }
